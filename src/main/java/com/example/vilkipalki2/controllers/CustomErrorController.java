7ceb3dc8f017083ccba12eeb36e1d06dae8bf1b7
Denis Perevertov <regicuinte@gmail.com>
2023-03-14 00:27:25 +0200

    added swagger again

--- src/main/java/com/example/vilkipalki2/controllers/CustomErrorController.java
+++ src/main/java/com/example/vilkipalki2/controllers/CustomErrorController.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class CustomErrorController implements ErrorController {
-
-    @GetMapping("/error")
-    public @ResponseBody String handleError(HttpServletRequest request) {
-
-        return "UNKNOWN ERROR";
-    }
-
-}
+//@Controller
+//public class CustomErrorController implements ErrorController {
+//
+//    @GetMapping("/error")
+//    public @ResponseBody String handleError(HttpServletRequest request) {
+//
+//        return "UNKNOWN ERROR";
+//    }
+//
+//}
